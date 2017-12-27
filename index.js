34c3ca7c1cca9695e5bd6fa1b762184ff45f05a2
fengxinming <fxm0016@126.com>
2017-12-28 01:52:32 +0800

    初步完成

--- index.js
+++ index.js
@@ -8,52 +8,47 @@ function convertError(err) {
   return createError(err, opts);
 }
 
-function convertMiddleware(fn) {
-  try {
-    return function(req, res, next) {
-      Promise.resolve(fn(req, res)).then(() => {
-        next();
-      }, (err) => {
-        next(convertError(err));
-      });
-    };
-  } catch (err) {
-    return function(req, res, next) {
-      next(convertError(err));
-    };
+function compose(middleware) {
+  return function(req, res, next) {
+    let index = -1;
+    return dispatch(0);
+
+    function dispatch(i) {
+      if (i <= index) {
+        return Promise.reject(new Error('next() called multiple times'));
+      }
+      index = i;
+      let fn = middleware[i];
+      if (i === middleware.length) {
+        fn = next;
+      }
+      if (!fn) {
+        return Promise.resolve();
+      }
+      try {
+        return Promise.resolve(fn(req, res, function next(err) {
+          if (err) {
+            return Promise.reject(err);
+          } else {
+            return dispatch(i + 1);
+          }
+        }));
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
   }
 }
 
 function createApplication() {
-  const middlewares = flatten(Array.from(arguments));
-  let index = -1;
-  middlewares.map((middleware) => {
-    return function(context, next) {
-      let index = -1
-      return dispatch(0);
-
-      function dispatch(i) {
-        if (i <= index) {
-          return Promise.reject(new Error('next() called multiple times'));
-        }
-        index = i;
-        let fn = middleware[i];
-        if (i === middleware.length) {
-          fn = next;
-        }
-        if (!fn) {
-          return Promise.resolve();
-        }
-        try {
-          return Promise.resolve(fn(context, function next() {
-            return dispatch(i + 1);
-          }));
-        } catch (err) {
-          return Promise.reject(err);
-        }
-      }
-    }
-  });
+  const middleware = flatten(Array.from(arguments));
+  return function(req, res, next) {
+    return compose(req, res, middleware).then(() => {
+      next();
+    }).catch((err) => {
+      next(convertError(err));
+    });
+  };
 }
 
 module.exports = createApplication;
