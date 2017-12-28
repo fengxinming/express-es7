2c48e93f7a73b51973d4eef762476d89bbff77d2
fengxinming <fxm0016@126.com>
2017-12-29 01:03:48 +0800

    添加路由支持

--- index.js
+++ index.js
@@ -1,53 +1,8 @@
 'use strict';
 
-const flatten = require('array-flatten');
-const createError = require('http-errors');
+const ea = require('./lib');
+const Router = require('./lib/router');
 
-function convertError(err) {
-  const opts = !err.status ? { status: 500 } : {};
-  return createError(err, opts);
-}
+ea.Router = Router;
 
-function compose(middleware) {
-  return function (req, res) {
-    let index = -1;
-    return dispatch(0);
-
-    function dispatch(i) {
-      if (i <= index) {
-        return Promise.reject(new Error('next() called multiple times'));
-      }
-      index = i;
-      let fn = middleware[i];
-      if (i === middleware.length) {
-        fn = undefined;
-      }
-      if (!fn) {
-        return Promise.resolve();
-      }
-      try {
-        return Promise.resolve(fn(req, res, function (err) {
-          if (err) {
-            return Promise.reject(err);
-          } else {
-            return dispatch(i + 1);
-          }
-        }));
-      } catch (err) {
-        return Promise.reject(err);
-      }
-    }
-  }
-}
-
-function createApplication() {
-  const middleware = flatten(Array.from(arguments));
-  const fnMiddleware = compose(middleware);
-  return function (req, res, next) {
-    return fnMiddleware(req, res).then(next).catch((err) => {
-      next(convertError(err));
-    });
-  };
-}
-
-module.exports = createApplication;
+module.exports = ea;
